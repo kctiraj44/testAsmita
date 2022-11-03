26a388ef51c3ba91a3b4e23cf93ab79614283c45
Tiraj KC <kctiraj44@gmail.com>
2022-11-03 18:23:56 -0500

    first commit

---
+++ AsmitaProj/src/com/asmita/Abc.java
@@ -0,0 +1,21 @@
+package com.asmita;
+
+public class Abc {
+
+	
+	
+public String getMoney() {
+	
+	return "I have a $10000";
+}
+
+
+
+public double getMoneyindolalr() {
+	
+	return 100000.0;
+}
+
+
+
+}
---
+++ HelloWorldApp/src/com/java/helloworld/ABC.java
@@ -0,0 +1,11 @@
+package com.java.helloworld;
+
+public class ABC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*--------Testting the local app 2 -------*");
+
+	}
+
+}
